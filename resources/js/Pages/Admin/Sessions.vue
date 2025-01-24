0249042f90317bbb77e42e8b5486ac04859192ff
CB009882 <113457321+CadmiumBeast@users.noreply.github.com>
2025-01-24 10:42:13 +0530

    Update Sessions.vue

--- resources/js/Pages/Admin/Sessions.vue
+++ resources/js/Pages/Admin/Sessions.vue
@@ -1,6 +1,7 @@
 <script setup>
  import AdminLayout from "@/Layouts/AdminLayout.vue";
  import UserCard from "@/Components/UserCard.vue";
+ import { computed, ref, watch } from "vue";
 
  const props = defineProps(
      {
@@ -11,12 +12,32 @@
      }
  )
 
+ const search = ref("");
+
+ const filteredUsers = computed(() => {
+     return props.users.filter((user) => {
+         const searchTerm = search.value.toLowerCase();
+         return (
+             user.name.toLowerCase().includes(searchTerm) ||
+             user.profile.cb_number?.toLowerCase().includes(searchTerm)
+         );
+     });
+ });
 </script>
 <template>
     <AdminLayout>
+        <!-- Search bar -->
+        <div class="m-12">
+            <input
+                type="text"
+                class="w-full border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+                placeholder="Search for users..."
+                v-model="search"
+            />
+        </div>
         <div class="m-12">
             <UserCard
-                v-for="user in props.users"
+                v-for="user in filteredUsers"
                 :key="user.id"
                 :username="user.name"
                 :user_id="user.id"
